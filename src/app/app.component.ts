b71140b54ff4afb466986d249ab8d8e9c5ae95c6
arekgotfryd <arekgofi@gmail.com>
2021-04-09 12:51:52 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { timer } from "rxjs/internal/observable/timer";
-import { filter, repeatWhen, switchMap, take, takeUntil } from "rxjs/operators";
-import { Observable, of, Subject } from "rxjs";
+import { Observable, of } from "rxjs";
 import { delay, tap, mergeMap, repeat } from "rxjs/operators";
 
 const completeWith = <T>(predicate: (arg: T) => boolean) => (
@@ -28,13 +26,9 @@ const completeWith = <T>(predicate: (arg: T) => boolean) => (
 })
 export class AppComponent {
   name = "Angular";
-  poll: any;
-  sub: any;
-
-  stop$ = new Subject();
 
   constructor(private http: HttpClient) {
-    this.poll = of({})
+    const poll = of({})
       .pipe(
         mergeMap(_ => this.request()),
         delay(3000),
@@ -45,29 +39,12 @@ export class AppComponent {
       )
       .pipe(tap(this.log));
 
-    this.sub = this.poll.subscribe();
+    poll.subscribe();
   }
-  // takeUntil(this.stop$),
 
   request() {
-    // now returns an Observable of Config
     return this.http.get<any>("https://jsonplaceholder.typicode.com/todos/1");
   }
-  //fakeDelayedRequest = () => of(new Date()).pipe(delay(1000));
-
-  delayedRequest = () => {
-    // return this.request().pipe(delay(1000));
-    return this.request();
-  };
-
-  checkResponse = resp => {
-    // console.log(resp);
-    if (resp.userId === 1) {
-      // this.sub.unsubscribe();
-      // return resp;
-      this.stop$.next();
-    }
-  };
 
   log = response => {
     console.log(response);
